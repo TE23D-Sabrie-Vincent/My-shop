3093ec9204ccd8b72b300e799ddfd3189f85d289
Vincent Sabrie <vincent.sabrie@elev.ga.ntig.se>
2024-10-04 10:56:05 +0200

    Beginnging of me shop  and yur name yes :D

---
+++ my shop/Program.cs
@@ -0,0 +1,28 @@
+﻿System.Console.WriteLine("Vad heter du??");
+
+string Namn;
+
+Namn = Console.ReadLine();
+
+if (Namn == "")
+{
+    System.Console.WriteLine("... Du har ett namn\n");
+}
+
+while (Namn == "")
+{
+    System.Console.WriteLine("Yu stupid, skriv ett namn");
+}
+
+while (Namn.Length >= 7 )
+{
+    System.Console.WriteLine("För långt namn, orkar inte");
+    Namn = Console.ReadLine();
+}
+
+int Playercash = 100;
+
+while (Playercash > 0)
+{
+    System.Console.WriteLine("Vad vill du skaffa???");
+}
